330bd1021ea3c5edcaf9e44eda57296c382f4ad4
Mateus Queiroz Correia <mateus_queiroz@hotmail.com>
2018-08-05 10:36:36 -0300

    Initial Commit

---
+++ src/main/java/guru/springframework/joke/JokeApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.joke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokeApplication.class, args);
+    }
+}
